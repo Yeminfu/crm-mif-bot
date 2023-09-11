bc3b220f12cd90f5f49655c711212ee20c4db681
Yeminfu <yeminfu4319@gmail.com>
2023-09-11 18:54:28 +1000

    картинка наконец отпралена в чат

--- tools/imageResender.js
+++ tools/imageResender.js
@@ -1,14 +1,13 @@
-import fs from "fs";
-import https from "https"
+const telegramToken = process.env.TELEGRAM_BOT_TOKEN;
+
 export default async function imageResender(msg, bot) {
   const chatId = msg.chat.id;
 
-  const token = process.env.TELEGRAM_BOT_TOKEN;
   if (msg.photo) {
     const photo = msg.photo[msg.photo.length - 1];
     const fileId = photo.file_id;
     bot.getFile(fileId).then((fileInfo) => {
-      const fileUrl = `https://api.telegram.org/file/bot${token}/${fileInfo.file_path}`;
+      const fileUrl = `https://api.telegram.org/file/bot${telegramToken}/${fileInfo.file_path}`;
       saveImage(fileUrl, 'saved_image.jpg').then(() => {
         bot.sendMessage(chatId, 'Изображение сохранено');
       }).catch((error) => {
@@ -21,16 +20,37 @@ export default async function imageResender(msg, bot) {
 }
 
 function saveImage(url, fileName) {
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(fileName);
-    const request = https.get(url, (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close(resolve);
+  return new Promise(async (resolve, reject) => {
+
+    const formData = new FormData();
+
+    formData.append('text', 'здравствуйте');
+    formData.append('essense', 'lead');
+    formData.append('essense_id', '1');
+
+    const blob = await fetch(url).then((response) => response.blob())
+
+    const file = new File([blob], 'fileName.jpeg', { type: blob.type });
+
+    formData.append('images', file);
+
+    await fetch(`${process.env.SERVER}/api/messages/send`, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        Cookie: `auth=1x4Wz1Rum0YsxxEtT7rR`
+      },
+    }).then(x => {
+      console.log('x.ok', x.ok);
+      console.log('x.status', x.status);
+      console.log('x.statusText', x.statusText);
+      return x.text();
+    })
+      .then(x => {
+        console.log('x', x);
       });
-    }).on('error', (error) => {
-      fs.unlink(fileName);
-      reject(error);
-    });
+
+    resolve('0');
+
   });
 }
