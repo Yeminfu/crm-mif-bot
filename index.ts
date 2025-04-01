27dabc56733983853a19b488ca307f63c8ba8c31
Yeminfu <yeminfu4319@gmail.com>
2025-04-01 19:38:29 +1000

    rm gb

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-console.log('hello');
+import mysql from 'mysql2';
 import 'dotenv/config';
 
+console.log('hello');
 const token = process.env.TOKEN;
-
 console.log(token);
+
+
+export const db_connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+});
