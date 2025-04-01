1b2d9baf8977043892351f59e157dff0ef957691
Yeminfu <yeminfu4319@gmail.com>
2025-04-01 20:30:59 +1000

    push

--- index.ts
+++ index.ts
@@ -17,6 +17,18 @@ console.log(token);
     password: process.env.DB_PASSWORD,
   });
 
+  const res = await getSales(connection);
+
+  const json = JSON.stringify(res);
+
+  sendMessage(5050441344, 'zzz', String(token))
+
+  console.log(res);
+
+
+  await connection.end();
+  return;
+
   try {
     // Выполнение запроса
     const [rows, fields] = await connection.execute('show tables');
@@ -37,3 +49,34 @@ console.log(token);
 
 
 
+
+
+async function getSales(connection: any): Promise<{ год: number, месяц: number, товар: string, магазин: string, 'к-во': number }[]> {
+  // const connection = await dbConnection();
+  const qs = `
+    select
+      P.name as товар
+      /*,P.idCategory idКатегории*/
+      /*,S.idShop*/
+      ,Sh.shopName as магазин
+      ,sum(S.count) AS 'к-во'
+      ,sum(S.sum) AS сумма
+    from chbfs_sales S
+      inner join chbfs_shops Sh on Sh.id = S.idShop
+        inner join chbfs_products P on P.id = S.idProduct
+          inner join chbfs_categories C on C.id = P.idCategory
+    where S.created_date >= CURDATE()
+      
+    group by
+      P.name 
+      /*,P.idCategory*/
+      ,S.idShop
+      ,Sh.shopName
+    order by магазин, товар;
+  `;
+  const res = await connection.query(qs)
+    .then(([x]: any) => x)
+  // .then(x => x[0]);
+  // await connection.end();
+  return res;
+}
